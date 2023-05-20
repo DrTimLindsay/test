55e38d36e0354f6d7422e506c03719fdc75a7d8c
DrTimLindsay <dr.tim.lindsay@gmail.com>
2023-05-20 17:46:31 -0400

    next

--- main.py
+++ main.py
@@ -1,33 +1,77 @@
 from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
-
-app = Dash(__name__)
-
-ALLOWED_TYPES = (
-    "text", "number", "password", "email", "search",
-    "tel", "url", "range", "hidden",
-)
+import plotly.express as px
+import dash_bootstrap_components as dbc
+# import datetime
 
 
-app.layout = html.Div(
-    [
-        dcc.Input(
-            id="input_{}".format(_),
-            type=_,
-            placeholder="input type {}".format(_),
-        )
-        for _ in ALLOWED_TYPES
-    ]
-    + [html.Div(id="out-all-types")]
-)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
+           meta_tags=[{'name': 'viewport',
+                       'content': 'width=device-width, initial-scale=1.0'}]
+           )
+
+methods = ['Horwill 4', 'Horwill 5', 'Canova', '% WR']
+
+
+# width={'size': 6, 'offset': 0, 'order': 1}
+# className='text-center text-primary mb4'
+
+
+# App layout
+
+
+app.layout = dbc.Container([
+    html.H2('Race Performances', className='text-center text-primary mt-2'),
+    html.P('Input any and all race performances that represent your current fitness.', className='text-center mb4'),
+    dbc.Row([
+        dbc.Col(html.P('800 m', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='t8', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify='center', align='center', className='mb-2'),
+    dbc.Row([
+        dbc.Col(html.P('1500 m', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='t15', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify = 'center', align = 'center', className = 'mb-2'),
+    dbc.Row([
+        dbc.Col(html.P('3000 m', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='t3', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify='center', align='center', className='mb-2'),
+    dbc.Row([
+        dbc.Col(html.P('5000 m', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='t5', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify='center', align='center', className='mb-2'),
+    dbc.Row([
+        dbc.Col(html.P('10,000 m', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='t10', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify='center', align='center', className='mb-2'),
+    dbc.Row([
+        dbc.Col(html.P('half marathon', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='tHalf', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify='center', align='center', className='mb-2'),
+    dbc.Row([
+        dbc.Col(html.P('marathon', className='text-end m-0'), width=2),
+        dbc.Col(dbc.Input(id='tFull', placeholder='hh:mm:ss', type='text'), width=2)
+    ],
+        justify='center', align='center', className='mb-5'),
+    html.H2('Pace Chart', className='text-center text-primary'),
+    dbc.Row(
+        dbc.Col(dcc.Dropdown(id='method', multi=False, value='choose method', options=methods), width=2),
+        justify='center', align='center', className='mb-2'),
+    html.Div(id='output'),
+])
 
 
 @app.callback(
-    Output("out-all-types", "children"),
-    [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
+    Output(component_id='output', component_property='children'),
+    Input(component_id='t8', component_property='value')
 )
-def cb_render(*vals):
-    return " | ".join((str(val) for val in vals if val))
+def update_output_div(input_value):
+    return f'Output: {input_value}'
 
 
 if __name__ == "__main__":
