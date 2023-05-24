e340f18b6d8a0cd575e1b47ad635b9f2250520de
DrTimLindsay <dr.tim.lindsay@gmail.com>
2023-05-23 20:36:16 -0400

    added README

--- src/app.py
+++ src/app.py
@@ -10,6 +10,7 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP],
            meta_tags=[{'name': 'viewport',
                        'content': 'width=device-width, initial-scale=1.0'}]
            )
+server = app.server
 
 methods = ['Horwill 4', 'Horwill 5', 'Canova', '% WR']
 
